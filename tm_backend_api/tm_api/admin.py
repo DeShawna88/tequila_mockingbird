3f2fa5223ea8a51f36823b80a510e0863fd15c8d
Shawnade <deshawnagalloway@yahoo.com>
2025-04-30 13:37:55 -0400

    updated admin.py

--- tm_backend_api/tm_api/admin.py
+++ tm_backend_api/tm_api/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Ingredient
 from .models import ShoppingList
+from .models import Recipe
+from .models import RecipeIngredient
+from .models import User
 
 # Register your models here.
 admin.site.register(Ingredient)
 admin.site.register(ShoppingList)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(User)
